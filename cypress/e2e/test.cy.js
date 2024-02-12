7752ac591306b92f5d3bda15b3d20ef4f3d35659
vijayakunwar <kunwar.vijaya@gmail.com>
2024-02-12 22:10:03 +1000

    attaching untrack file

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,9 @@
-describe('test', () => {
-    it('passes', () => {
+describe('test suit', () => {
+    it('test case 1', () => {
         cy.log("test message")
+    })
+
+    it('test case 2',()=>{
+        cy.log("test case 2")
     })
 })
